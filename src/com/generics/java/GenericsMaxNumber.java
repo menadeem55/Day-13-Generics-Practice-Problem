27e10362e365f7d7871c3546d7e967538dfa0746
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-08 00:54:20 +0530

    [Added: Taking More than 3 Parameters]

--- src/com/generics/java/GenericsMaxNumber.java
+++ src/com/generics/java/GenericsMaxNumber.java
@@ -1,30 +1,35 @@
 package com.generics.java;
 
 public class GenericsMaxNumber<T> {
-		T num1, num2, num3;
-		public GenericsMaxNumber(T num1, T num2, T num3) {
+		T num1, num2, num3, num4;
+		public GenericsMaxNumber(T num1, T num2, T num3, T num4) {
 			this.num1 = num1;
 			this.num2 = num2;
 			this.num3 = num3;
+			this.num4 = num4;
 		}
 
-	public static <T extends Comparable<T>> T maximumAmong3(T num1, T num2, T num3) {
+	public static <T extends Comparable<T>> T maximumAmong4(T num1, T num2, T num3, T num4) {
 
 		T max = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			max = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
 			max = num2;
-		else
+		else if (num3.compareTo(num4) > 0)
 			max = num3;
+		else
+			max = num4;
 		return max;
 	}
 
 	public static void main(String[] args) {
 
-		System.out.println("The max Number Among 3 String is: \n " + maximumAmong3("Mango", "Apple", "Banana"));
-		System.out.println("The max Number Among 3 Integer number is : \n " + maximumAmong3(25, 38, 59));
-		System.out.println("The max Number Among 3 Float number is: \n" + maximumAmong3(33.5, 29.44, 77.25));
+		
+		
+		System.out.println("The max Number Among 3 String is: \n " + maximumAmong4("Mango", "Apple", "Banana", "Orange"));
+		System.out.println("The max Number Among 3 Integer number is : \n " + maximumAmong4(25, 38, 59, 88));
+		System.out.println("The max Number Among 3 Float number is: \n" + maximumAmong4(33.5f, 29.44f, 77.25f, 44.55f));
 	}
 
 }
