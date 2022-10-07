33e1103d5a3a4e063b82f47faffe987e1103d58d
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-08 00:16:11 +0530

    [Added: Getting Max Float Number]

--- src/com/generics/java/GenericsMaxNumber.java
+++ src/com/generics/java/GenericsMaxNumber.java
@@ -2,9 +2,9 @@ package com.generics.java;
 
 public class GenericsMaxNumber {
 
-	public static Integer maximumInteger(Integer num1, Integer num2, Integer num3) {
+	public static Float maximumFloat(Float num1, Float num2, Float num3) {
 
-		Integer max = num1;
+		Float max = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			max = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -16,7 +16,7 @@ public class GenericsMaxNumber {
 
 	public static void main(String[] args) {
 
-		System.out.println("Maximum Number Among 3 will be: " + maximumInteger(77, 45, 55));
+		System.out.println("Maximum Number Among 3 will be: " + maximumFloat(77.55f, 45.12f, 55.51f));
 
 	}
 
