68bf8f09bc5c0bd17c0173779b995a6c75487708
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-08 00:23:33 +0530

    [Added: Getting Max String]

--- src/com/generics/java/GenericsMaxNumber.java
+++ src/com/generics/java/GenericsMaxNumber.java
@@ -2,9 +2,9 @@ package com.generics.java;
 
 public class GenericsMaxNumber {
 
-	public static Float maximumFloat(Float num1, Float num2, Float num3) {
+	public static String maximumString(String num1, String num2, String num3) {
 
-		Float max = num1;
+		String max = num1;
 		if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
 			max = num1;
 		else if (num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -16,7 +16,7 @@ public class GenericsMaxNumber {
 
 	public static void main(String[] args) {
 
-		System.out.println("Maximum Number Among 3 will be: " + maximumFloat(77.55f, 45.12f, 55.51f));
+		System.out.println("Maximum Number Among 3 will be: " + maximumString("Mango", "Apple", "Banana"));
 
 	}
 
